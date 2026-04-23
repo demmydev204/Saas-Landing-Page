c980a80caba45f4990a45b4cd979dc7ac64f58b7
Build With Chuks <seunanthony56@gmail.com>
2026-04-23 16:57:43 -0700

    commit testimonaials

--- js/saas.js
+++ js/saas.js
@@ -6,4 +6,18 @@ mobilemenu.addEventListener('click', ()=>{
 mobilemenu.classList.toggle('active');
 bars.classList.toggle('active');
 
-})
+});
+
+    const swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
